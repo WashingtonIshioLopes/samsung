aa0ff191b167ddd5eea61c3645c88e2320f41ca4
Washington Ishio Lopes <washington@cis.ind.br>
2024-07-01 12:43:42 -0300

    Incluido key

--- catalogo-front/src/Components/Products/CatalogProducts.jsx
+++ catalogo-front/src/Components/Products/CatalogProducts.jsx
@@ -9,6 +9,7 @@ import BASE_URL from '../../config';
 const CatalogProducts = (props) => {
 
     const [products, setProducts] = useState([]);
+    const [filter, setFilter] = useState();
 
     const { token } = props;
 
@@ -49,8 +50,37 @@ const CatalogProducts = (props) => {
 
     }, []);
 
-    const handleClick = () => {
-        console.log('Botão clicado!');
+    const handleClickFavorites = () => {
+        console.log('Click Favorites!');
+    }
+
+    const handleClickFilter = () => {
+        console.log('Click Filter!');
+
+        const fetchData = async () => {
+            try {
+                
+                console.log(BASE_URL  + '/products');
+                const response = await axios.get(BASE_URL  + '/products', config);
+
+                if (response.status === 200) {
+                    console.log('Lista de Produtos.');
+                    console.log(response.data);
+                    setProducts(response.data);
+                } else {
+                    alert('Erro em busca Produtos. Por favor, tente novamente.');
+                }
+            } catch (error) {
+                console.error('Erro ao buscar dados:', error);
+            }
+        };
+
+        fetchData();
+
+    }
+
+    const handleClickDetails = () => {
+        console.log('Click Details!');
     }
 
     return (
@@ -64,7 +94,7 @@ const CatalogProducts = (props) => {
                         <h3 className="mb-3">Pesquisa</h3>
                     </div>
                     <div className="col-md-4">
-                        <button type="button" className="btn btn-sm btn-outline-secondary px-5 mb-5 w-100" onClick={handleClick} >Meus Favoritos</button>
+                        <button type="button" className="btn btn-sm btn-outline-secondary px-5 mb-5 w-100" onClick={handleClickFavorites} >My Favorites</button>
                     </div>
                 </div>
                 
@@ -72,12 +102,12 @@ const CatalogProducts = (props) => {
                     <form className="d-flex align-items-end w-100">
                         <div className="col-md-11">
                             <div className="form-group mb-0">
-                                <label>Filtro:</label>
+                                <label>Filter:</label>
                                 <input type="text" className="form-control" id="pesquisa" aria-describedby="filterHelp" placeholder="Filter" onChange={(e) => setFilter(e.target.value)}/>
                             </div>
                         </div>
                         <div className="col-md-1 d-flex align-items-end">
-                            <button className="btn btn-primary ml-20" onClick={handleClick} >Filtrar</button>
+                            <button className="btn btn-primary ml-20" onClick={handleClickFilter} >Filtrar</button>
                         </div>
                     </form>
                 </div>
@@ -88,7 +118,7 @@ const CatalogProducts = (props) => {
 
                     {products.map(product => (
 
-                        <div className="col-md-4">
+                        <div className="col-md-4" key={ product.id }>
                             <div className="card mb-4 shadow-sm">
                                 <img className="card-img-top" src={product.images[0].image} alt="Card image cap" />
                                 <div className="card-body">
@@ -96,11 +126,11 @@ const CatalogProducts = (props) => {
                                     <p className="card-text">{ product.description }</p>
                                     <div className="d-flex justify-content-between align-items-center">
                                         <div className="btn-group">
-                                            <button type="button" className="btn btn-sm btn-outline-secondary" onClick={handleClick} >Detalhes</button>
+                                            <button type="button" className="btn btn-sm btn-outline-secondary" onClick={handleClickDetails} >Detalhes</button>
                                         </div>
                                         <small className="text-muted">{ product.weight }</small>
                                         <div className="icons-container">
-                                            <i className="fa fa-star" title="Favorito"></i>
+                                            {/* <i className="fa fa-star" title="Favorito"></i> */}
                                         </div>
                                     </div>
                                 </div>
