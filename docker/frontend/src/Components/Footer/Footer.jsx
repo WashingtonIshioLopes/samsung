64cbdee91ac69941c09f380b45ac567e3051ebf7
Washington Ishio Lopes <washington@cis.ind.br>
2024-07-03 08:10:04 -0300

    Incluindo docker

---
+++ docker/frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary text-black py-3">
+      <div className="container text-center">
+          <p>&copy; 2024 Washington Ishio Lopes. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ docker/frontend/src/config.jsx
@@ -0,0 +1,3 @@
+const BASE_URL = 'http://localhost:8080/samsung/api/v1_0'; // URL base da sua API
+
+export default BASE_URL;
